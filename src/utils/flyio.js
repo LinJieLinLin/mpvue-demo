5720943ea27ba2030f5a6223896c3c540075b39b
linjielinlin <993353454@qq.com>
2019-03-05 10:33:24 +0800

    修改flyio.js

--- src/utils/flyio.js
+++ src/utils/flyio.js
@@ -1,85 +1,30 @@
-import { needLogin, toPage, toast } from './wx'
-var Fly = require('flyio/dist/npm/wx')
-var fly = new Fly()
-  // 默认5s超时
-fly.config.timeout = 5000
-fly.config.baseURL = 'http://yapi.demo.qunar.com/mock/9343'
+import fly from 'flyio'
+import { Loading } from './jClass'
+import { Indicator, Toast } from 'mint-ui'
 const suCode = ',0,'
-let requestNum = 0
-let timmer = null
-let temTime = null
-let temStartTime = null
-
-/**
- * @description loading处理
- * @function
- * @param {boolean} isAdd 是否增加请求数
- * @returns {string}
- */
-export const loading = (isAdd) => {
-  if (isAdd) {
-    if (!requestNum) {
-      temTime = +new Date()
-      timmer = setTimeout(function() {
-        temStartTime = +new Date()
-        console.info('显示loading用时：', temStartTime - temTime)
-        wx.showLoading({})
-      }, 300)
-    }
-    requestNum++
-  } else {
-    requestNum--
-  }
-  console.log(requestNum)
-  if (!requestNum) {
-    clearTimeout(timmer)
-    setTimeout(function() {
-      wx.hideLoading()
-      if (temStartTime) {
-        console.info('显示loading时长：', +new Date() - temStartTime)
-      }
-    }, 300)
-  }
-}
-
-// console.log('环境变量：', process.env)
+fly.config.timeout = process.env.VUE_APP_TIMEOUT || 10000
+fly.config.baseURL = process.env.BASE_URL
+let L = new Loading(Indicator.open, Indicator.close)
 // 添加请求拦截器
-fly.interceptors.request.use((request) => {
-  loading(1)
-    // 判断登录
-  needLogin().then(
-    rs => {
-      loading()
-      if (getApp && getApp().globalData) {
-        getApp().globalData.code = rs.code
-      }
-      console.log(rs.code)
-      toPage('login')
-    },
-    err => {
-      // 已登录
-      console.log('已登录', err)
-    }
-  )
-
+fly.interceptors.request.use(request => {
+  L.loading(1)
   // 给所有请求添加自定义header
   // request.headers['X-Tag'] = 'flyio'
   // 打印出请求体
   console.log('请求拦截', request)
-    // 终止请求
-    // var err=new Error("xxx")
-    // err.request=request
-    // return Promise.reject(new Error(""))
+  // 终止请求
+  // var err=new Error("xxx")
+  // err.request=request
+  // return Promise.reject(new Error(""))
   return request
 })
 
 // 添加响应拦截器，响应拦截器会在then/catch处理之前执行
 fly.interceptors.response.use(
-  (res) => {
-    loading()
-
+  res => {
+    L.loading()
     console.log('响应拦截:', res)
-      // const data = JSON.parse(res.data)
+    // const data = JSON.parse(res.data)
     const data = res.data
     if (suCode.match(',' + data.code + ',')) {
       return data.data
@@ -88,36 +33,37 @@ fly.interceptors.response.use(
     switch (data.code) {
       case 4:
         console.log('请授权登录', data.data.url)
+        location.href = data.data.url
         return Promise.reject(data.data)
       default:
         return Promise.reject(data.data)
     }
   },
-  (err) => {
-    loading()
+  err => {
+    L.loading()
 
     // 发生网络错误后会走到这里
     console.log('http err:', err)
-      // 超时处理
+    // 超时处理
     if (err && err.message && err.message.match('timeout')) {
       console.log(err.message)
-      toast('请求超时！')
+      Toast('请求超时！')
       return Promise.reject(err)
     }
     // 状态码判断
     switch (err.status) {
       case 404:
-        toast('请求失败,请重试')
+        Toast('请求失败,请重试')
         break
       case 500:
-        toast('服务器错误,请重试')
+        Toast('服务器错误,请重试')
         break
     }
     // 发生网络错误后reject
     return Promise.reject(err)
   }
 )
-export const post = (argData) => {
+export const post = argData => {
   if (!argData) {
     return
   }
@@ -126,7 +72,7 @@ export const post = (argData) => {
     config: argData.config
   })
 }
-export const get = (argData) => {
+export const get = argData => {
   if (!argData) {
     return
   }
@@ -135,7 +81,7 @@ export const get = (argData) => {
     config: argData.config
   })
 }
-export const request = (argData) => {
+export const request = argData => {
   if (!argData) {
     return
   }
